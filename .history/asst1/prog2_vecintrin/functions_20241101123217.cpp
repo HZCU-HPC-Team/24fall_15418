f4cb36bc3e51c991786fccd0ab105466e0731389
Bao Zhuhan <baozhuhan2004@163.com>
2024-11-03 13:59:56 +0800

    update asst1-prog2

--- .history/asst1/prog2_vecintrin/functions_20241101123217.cpp
+++ .history/asst1/prog2_vecintrin/functions_20241101123217.cpp
@@ -93,40 +93,8 @@ void clampedExpSerial(float *values, int *exponents, float *output, int N)
 
 void clampedExpVector(float *values, int *exponents, float *output, int N)
 {
-	__cmu418_vec_float x;
-	__cmu418_vec_float result;
-	__cmu418_vec_float xpower = x;
-	__cmu418_vec_int y;
-	__cmu418_mask maskAll, maskIsPositive, maskIsNotZero, maskIsGreaterThanOne;
-	__cmu418_vec_float vecMax = _cmu418_vset_float(4.18f);
-	__cmu418_vec_float vecOne = _cmu418_vset_float(1.0f);
-	__cmu418_vec_int vecZero = _cmu418_vset_int(0);
-	__cmu418_vec_int vecOneInt = _cmu418_vset_int(1);
-	for (int i = 0; i < N; i += VECTOR_WIDTH)
-	{
-		maskAll = _cmu418_init_ones();
-		_cmu418_vload_float(x, values + i, maskAll);
-		_cmu418_vload_int(y, exponents + i, maskAll);
-		_cmu418_vset_float(result, 1.0f, maskAll);
-		_cmu418_vmove_float(xpower, x, maskAll);
-
-		_cmu418_vgt_int(maskIsPositive, y, vecZero, maskAll);
-		while (_cmu418_cntbits(maskIsPositive) > 0)
-		{
-			__cmu418_vec_int vecIsNotZero;
-			_cmu418_vbitand_int(vecIsNotZero, y, vecOneInt, maskIsPositive);
-			maskIsNotZero = _cmu418_init_ones(0);
-			_cmu418_vgt_int(maskIsNotZero, vecIsNotZero, vecZero, maskIsPositive);
-			_cmu418_vmult_float(result, result, xpower, maskIsNotZero);
-			_cmu418_vmult_float(xpower, xpower, xpower, maskIsPositive);
-			_cmu418_vshiftright_int(y, y, vecOneInt, maskIsPositive);
-			_cmu418_vgt_int(maskIsPositive, y, vecZero, maskAll);
-		}
-
-		_cmu418_vgt_float(maskIsGreaterThanOne, result, vecMax, maskAll);
-		_cmu418_vset_float(result, 4.18f, maskIsGreaterThanOne);
-		_cmu418_vstore_float(output + i, result, maskAll);
-	}
+	// Implement your vectorized version of clampedExpSerial here
+	//  ...
 }
 
 float arraySumSerial(float *values, int N)
@@ -144,20 +112,7 @@ float arraySumSerial(float *values, int N)
 // Assume VECTOR_WIDTH is a power of 2
 float arraySumVector(float *values, int N)
 {
-	__cmu418_vec_float vecSum = _cmu418_vset_float(0.f);
-	__cmu418_vec_float vecVal;
-	__cmu418_mask maskAll = _cmu418_init_ones();
-
-	for (int i = 0; i < N; i += VECTOR_WIDTH)
-	{
-		_cmu418_vload_float(vecVal, values + i, maskAll);
-		_cmu418_vadd_float(vecSum, vecSum, vecVal, maskAll);
-	}
-
-	float sum = 0.f;
-	for (int i = 0; i < VECTOR_WIDTH; i++)
-	{
-		sum += vecSum.value[i];
-	}
-	return sum;
+	// Implement your vectorized version here
+	//  ...
+	return 0.f;
 }
